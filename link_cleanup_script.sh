4fcc520be8c74b960d3e2ba3eb7a9e4e19373c55
Robert Abramov <74544526+robiik7@users.noreply.github.com>
2025-05-14 00:55:20 -0400

    Update link_cleanup_script.sh

--- link_cleanup_script.sh
+++ link_cleanup_script.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+# Remap links to new target domain
 
+target="$TARGET_DOMAIN"
+root="$OUTPUT_DIR"
+
+find "$OUTPUT_DIR" -type f \( -name "*.html" -o -name "*.css" -o -name "*.js" \) \
+  -exec sed -i '' 's~https://permittestpractice.com~https://'$target'~g' {} +
+
+echo "Remapping complete. All links now point to https://$target"
